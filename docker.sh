a31e39451adb5397a6c6915e3a532a5a39b2051e
Kaiyuan Liu <3395795+lkytal@users.noreply.github.com>
2023-02-03 00:58:14 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-apt-get install     ca-certificates     curl     gnupg     lsb-release
+sudo apt-get install     ca-certificates     curl     gnupg     lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -6,5 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
