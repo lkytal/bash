8a105c6cff22ac6b2f8951a78ee4d329186693e7
Kaiyuan Liu <3395795+lkytal@users.noreply.github.com>
2024-11-09 21:42:23 -0800

    Update db.py

--- db.py
+++ db.py
@@ -10,3 +10,15 @@ wget https://raw.githubusercontent.com/lkytal/bash/main/.zshrc -O .zshrc
 git clone https://github.com/wting/autojump.git
 cd autojump
 ./install.py
+
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh -b
+
+./miniforge3/bin/conda init zsh
+
+conda create -n ue python=3.10 -y
+conda activate ue
+
+pip install tensorflow==2.14.1
+pip install tensorflow_io
+pip install boto3
